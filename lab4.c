a94ccef51e0c73f17fd2e7880b155204da5c9d56
Dr. Rata Blanca <verardi.gino@gmail.com>
2022-01-08 20:57:02 -0300

    se corregen errores de intercambiar elementos
    nico gay

--- lab4.c
+++ lab4.c
@@ -22,61 +22,22 @@ void Quicksort_x();
 void add_element(LE *lista, long x, long y);
 void leer_archivo(LE *lista);
 void view_list(LE *lista);
-void intercambiar(Bloque *elemento1, Bloque *elemento2);
+void intercambiar(Bloque *elemento1, Bloque *elemento2, LE *lista);
 int main()
 {
-    Bloque *p1, *p2, *p3, *p4, *p5;
     LE *Lista;
-    long p1x = 5, p1y = 10, p2x = 10, p2y = 5, p3x = -3, p3y = 3, p4x = -4, p4y = 5, p5x = -1, p5y = 0;
+    
     Lista = (LE *)malloc(sizeof(LE));
-    Lista->init=NULL;
-    Lista->end=NULL;
+    Lista->init = NULL;
+    Lista->end = NULL;
 
-  /*  p1 = (Bloque *)malloc(sizeof(Bloque));
-    p2 = (Bloque *)malloc(sizeof(Bloque));
-    p3 = (Bloque *)malloc(sizeof(Bloque));
-    p4 = (Bloque *)malloc(sizeof(Bloque));
-    p5 = (Bloque *)malloc(sizeof(Bloque));
-    Lista = (LE *)malloc(sizeof(LE));
 
-    p1->x = p1x;
-    p1->y = p1y;
-    p1->pre = NULL;
-    p1->post = p2;
-
-    p2->x = p2x;
-    p2->y = p2y;
-    p2->pre = p1;
-    p2->post = p3;
-
-    p3->x = p3x;
-    p3->y = p3y;
-    p3->pre = p2;
-    p3->post = p4;
-
-    p4->x = p4x;
-    p4->y = p4y;
-    p4->pre = p3;
-    p4->post = p5;
-
-    p5->x = p5x;
-    p5->y = p5y;
-    p5->pre = p4;
-    p5->post = NULL;
-
-    Lista->n = 5;
-    Lista->init = p1;
-    Lista->end = p5;
-*/
-    //view_list(Lista);
     leer_archivo(Lista);
-
-    /*
-    add_element(Lista,  1,  4);
-    add_element(Lista,  2,  3);
-    add_element(Lista,  3,  2);
-    add_element(Lista,  4,  1);*/
     view_list(Lista);
+    intercambiar(Lista->init->post->post,Lista->end->pre->pre,Lista);
+    printf("\n\n\n");
+    view_list(Lista);
+
     return 0;
 }
 
@@ -96,8 +57,8 @@ void leer_archivo(LE *lista)
     long x, y;
     int n = 0;
 
-    //printf("Ingrese nombre del archivo (con extension, ejemplo: matriz.txt): ");
-   // scanf("%s", nombreArchivo);
+    // printf("Ingrese nombre del archivo (con extension, ejemplo: matriz.txt): ");
+    // scanf("%s", nombreArchivo);
 
     archivo = fopen("archivo1", "r");
 
@@ -107,7 +68,7 @@ void leer_archivo(LE *lista)
     }
     else
     {
-        while ((bus = fgetc(archivo)) != '\n' )
+        while ((bus = fgetc(archivo)) != '\n')
         {
             if (bus != ' ' || bus != '\n')
                 n = n * 10 + (int)bus - 48;
@@ -115,18 +76,18 @@ void leer_archivo(LE *lista)
 
         for (int i = 0; i < n; i++)
         {
-            x=0, y=0;
+            x = 0, y = 0;
             while ((bus = fgetc(archivo)) != ' ')
             {
                 // if (bus != '\n')
-                x = x * 10 + (long )bus - 48;
+                x = x * 10 + (long)bus - 48;
             }
             while ((bus = fgetc(archivo)) != '\n')
             {
                 // if (bus != '\n')
-                y = y * 10 + (long )bus - 48;
+                y = y * 10 + (long)bus - 48;
             }
-            add_element(lista,  x,  y);
+            add_element(lista, x, y);
         }
     }
 
@@ -135,10 +96,10 @@ void leer_archivo(LE *lista)
 
 void add_element(LE *lista, long x, long y)
 {
-    //constructor elemento
+    // constructor elemento
     Bloque *elemento = (Bloque *)malloc(sizeof(Bloque));
     elemento->x = x;
-    elemento->y =  y;
+    elemento->y = y;
     elemento->pre = NULL;
     elemento->post = NULL;
 
@@ -152,44 +113,50 @@ void add_element(LE *lista, long x, long y)
     {
 
         elemento->pre = lista->end;
-        elemento->pre->post=elemento;
+        elemento->pre->post = elemento;
         elemento->post = NULL;
         lista->end = elemento;
         lista->n = lista->n + 1;
-
     }
 }
 
-void view_list(LE *lista){
+void view_list(LE *lista)
+{
 
     Bloque *aux = lista->init;
     for (int i = 0; i < lista->n; i++)
     {
         printf("(x, y) = (%ld, %ld)\n", aux->x, aux->y);
         aux = aux->post;
-    }    
+    }
 }
 
+void intercambiar(Bloque *elemento1, Bloque *elemento2, LE *lista)
+{
 
-void intercambiar(Bloque *elemento1, Bloque *elemento2){
+    Bloque *post1 = elemento1->post;
+    Bloque *pre1 = elemento1->pre;
 
-    Bloque *post1=elemento1->post;
-    Bloque *pre1=elemento1->pre;
+    Bloque *post2 = elemento2->post;
+    Bloque *pre2 = elemento2->pre;
 
-    Bloque *post2=elemento2->post;
-    Bloque *pre2=elemento2->pre;
+    post1->pre = elemento2;
+    pre1->post = elemento2;
+    elemento2->post = post1;
+    elemento2->pre = pre1;
 
-    post1->pre=elemento2;
-    pre1->post=elemento2;
-    elemento2->post=post1;
-    elemento2->pre=pre1;
+    post2->pre = elemento1;
+    pre2->post = elemento1;
+    elemento1->post = post2;
+    elemento1->pre = pre2;
 
+    if (elemento1->post == NULL)
+        lista->end = elemento1;
+    if (elemento1->pre == NULL)
+        lista->init = elemento1;
 
-    post2->pre=elemento1;
-    pre2->post=elemento1;
-    elemento1->post=post2;
-    elemento1->pre=pre2;
-
+    if (elemento2->post == NULL)
+        lista->end = elemento2;
+    if (elemento2->pre == NULL)
+        lista->init = elemento2;
 }
-
-
