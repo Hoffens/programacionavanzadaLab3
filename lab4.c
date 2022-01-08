1a0b7a07dc9fb0d76086502de3355637b8d1635c
Dr. Rata Blanca <verardi.gino@gmail.com>
2022-01-08 03:39:06 -0300

    nuevas funciones
    se añadio add_element no funca.... se añade view y si fonca

--- lab4.c
+++ lab4.c
@@ -1,53 +1,54 @@
-#include <stdio.h> /* esenciales para la comunicacion con el programa */
+#include <stdio.h>  /* esenciales para la comunicacion con el programa */
 #include <stdlib.h> /* generador aleatorio basico, control de memoria, conversiones basicas, control de procesos */
 //#include <math.h> /* funciones matematicas basicas como floor, ceil, log2, etc. */
 #include <time.h> /* medicion de tiempo */
-
 
 typedef struct Bloque
 {
     long x;
     long y;
-    struct Bloque *pre;     // bloque anterior
-    struct Bloque *post;    // bloque siguiente
+    struct Bloque *pre;  // bloque anterior
+    struct Bloque *post; // bloque siguiente
 } Bloque;
 
-typedef struct LE   // lista enlazada
+typedef struct LE // lista enlazada
 {
-    Bloque *init;   // primer elemento de la lista
-    Bloque *end;    // ultimo elemento de la lista
-    int n;          // largo n de la lista
+    Bloque *init; // primer elemento de la lista
+    Bloque *end;  // ultimo elemento de la lista
+    int n;        // largo n de la lista
 } LE;
 
-
 void Quicksort_x();
-
+void add_element(LE *lista, long x, long y);
+void leer_archivo(LE *lista);
+void view_list(LE *lista);
 
 int main()
 {
     Bloque *p1, *p2, *p3, *p4, *p5;
     LE *Lista;
     long p1x = 5, p1y = 10, p2x = 10, p2y = 5, p3x = -3, p3y = 3, p4x = -4, p4y = 5, p5x = -1, p5y = 0;
+    Lista = (LE *)malloc(sizeof(LE));
+    Lista->init=NULL;
+    Lista->end=NULL;
 
-    p1 = (Bloque *) malloc(sizeof(Bloque));
-    p2 = (Bloque *) malloc(sizeof(Bloque));
-    p3 = (Bloque *) malloc(sizeof(Bloque));
-    p4 = (Bloque *) malloc(sizeof(Bloque));
-    p5 = (Bloque *) malloc(sizeof(Bloque));
-    Lista = (LE *) malloc(sizeof(LE));
+  /*  p1 = (Bloque *)malloc(sizeof(Bloque));
+    p2 = (Bloque *)malloc(sizeof(Bloque));
+    p3 = (Bloque *)malloc(sizeof(Bloque));
+    p4 = (Bloque *)malloc(sizeof(Bloque));
+    p5 = (Bloque *)malloc(sizeof(Bloque));
+    Lista = (LE *)malloc(sizeof(LE));
 
     p1->x = p1x;
     p1->y = p1y;
     p1->pre = NULL;
     p1->post = p2;
 
-
     p2->x = p2x;
     p2->y = p2y;
     p2->pre = p1;
     p2->post = p3;
 
-    
     p3->x = p3x;
     p3->y = p3y;
     p3->pre = p2;
@@ -65,33 +66,34 @@ int main()
 
     Lista->n = 5;
     Lista->init = p1;
-    Lista->end = p5;    
+    Lista->end = p5;
+*/
+    //view_list(Lista);
+    //leer_archivo(Lista);
 
 
-
-    Bloque *aux = Lista->init;
-    for (int i = 0; i < Lista->n; i++)
-    {
-        printf("(x, y) = (%ld, %ld)\n", aux->x, aux->y);
-        aux = aux->post;
-
-    }
-
+    add_element(Lista,  1,  2);
+    add_element(Lista,  2,  2);
+    add_element(Lista,  3,  2);
+    add_element(Lista,  4,  52);
+    view_list(Lista);
     return 0;
 }
 
 void Quicksort_x()
 {
-
 }
 
-void leer_archivo(LE *lista){
+void leer_archivo(LE *lista)
+{
+
+    Bloque *elem1, *elem2;
 
     char nombreArchivo[30];
     FILE *archivo;
 
     char bus;
-    long x,y;
+    long x, y;
     int n = 0;
 
     printf("Ingrese nombre del archivo (con extension, ejemplo: matriz.txt): ");
@@ -99,38 +101,72 @@ void leer_archivo(LE *lista){
 
     archivo = fopen(nombreArchivo, "r");
 
-    
-
     if (archivo == NULL)
     {
         printf("\nError de apertura del archivo. \n\n");
     }
     else
     {
-        while ((bus = fgetc(archivo)) != ' ' || bus!='\n')
+        while ((bus = fgetc(archivo)) != ' ' || bus != '\n')
         {
-            if (bus != ' ' || bus!='\n')
+            if (bus != ' ' || bus != '\n')
                 n = n * 10 + (int)bus - 48;
         }
-        lista->n=n;
-        
 
-        for (int i=0; i<n; i++)
+        for (int i = 0; i < n; i++)
         {
-        while ((bus = fgetc(archivo)) != ' ' )
-        {
-            // if (bus != '\n')
-            x = x * 10 + (long long)bus - 48;
+            while ((bus = fgetc(archivo)) != ' ')
+            {
+                // if (bus != '\n')
+                x = x * 10 + (long long)bus - 48;
+            }
+            while ((bus = fgetc(archivo)) != ' ' || bus != '\n')
+            {
+                // if (bus != '\n')
+                y = y * 10 + (long long)bus - 48;
+            }
+            add_element(lista,  x,  y);
         }
-        while ((bus = fgetc(archivo)) != ' ' || bus!='\n')
-        {
-            // if (bus != '\n')
-            y = y * 10 + (long long)bus - 48;
-        }
-        
-        
-        }
+    }
 
-        
+    fclose(archivo);
+}
+
+void add_element(LE *lista, long x, long y)
+{
+    //constructor elemento
+    Bloque *elemento = (Bloque *)malloc(sizeof(Bloque));
+    elemento->x = x;
+    elemento->y =  y;
+    elemento->pre = NULL;
+    elemento->post = NULL;
+
+    if (lista->init == NULL)
+    {
+        lista->init = elemento;
+        lista->end = elemento;
+        lista->n = 1;
+    }
+    else
+    {
+
+        elemento->pre = lista->end;
+        elemento->post = NULL;
+        lista->end = elemento;
+        lista->n = lista->n + 1;
+
     }
 }
+
+void view_list(LE *lista){
+
+    Bloque *aux = lista->init;
+    for (int i = 0; i < lista->n; i++)
+    {
+        printf("(x, y) = (%ld, %ld)\n", aux->x, aux->y);
+        aux = aux->post;
+    }    
+}
+
+
+
