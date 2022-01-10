44a6ad0911800ff51487cd24f130a7915bb43c5c
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-09 21:16:14 -0300

    se agrega menu, generacion random de puntos, tiempos

--- lab4.c
+++ lab4.c
@@ -3,6 +3,7 @@
 //#include <math.h> /* funciones matematicas basicas como floor, ceil, log2, etc. */
 #include <time.h> /* medicion de tiempo */
 
+#define NMAX 10000000
 
 typedef struct Bloque
 {
@@ -31,14 +32,184 @@ Bloque *Dividir_y(Bloque *left, Bloque *right);
 LE *sublistado(LE *lista, long cota_inf, long cota_sup, int x);
 // Agrega un elemento a la lista
 void add_element(LE *lista, long x, long y);
+// Genera un punto aleatorio y lo agrega a una lista
+void add_random_element(LE *lista);
 // Lee un archivo y carga sus puntos a una lista
-void leer_archivo(LE *lista);
+int leer_archivo(LE *lista);
 // Imprime en pantalla una lista
 void view_list(LE *lista);
 
 
 int main()
 {
+    // falta liberar memoria
+    srand(time(NULL));
+    int i, sel, isOk, n;
+    long cota_inf, cota_sup;
+    double t0, t1, time1;
+    LE *Lista;
+    Lista = (LE *)malloc(sizeof(LE));
+    Lista->init = NULL;
+    Lista->end = NULL;
+    
+    printf("Elegir el tipo de operacion a ejecutar:\n");
+    printf("1) Leer puntos desde archivo.\n");
+    printf("2) Generar puntos aleatorios.\n");
+    printf("3) Ordenar listado de puntos por coordenada x.\n");
+    printf("4) Ordenar listado de puntos por coordenada y.\n");
+    printf("5) Sublistado que satisface cotas en coordenada x.\n");
+    printf("6) Sublistado que satisface cotas en coordenada y.\n");
+    printf("9) Salir del programa.\n");
+    sel = 0;
+
+    do
+    {   
+        printf("Seleccion (0 para repetir el menu): ");
+        scanf("%d",&sel);
+        switch (sel)
+        {   
+            case 0:
+                printf("Elegir el tipo de operacion a ejecutar:\n");
+                printf("1) Leer puntos desde archivo.\n");
+                printf("2) Generar puntos aleatorios.\n");
+                printf("3) Ordenar listado de puntos por coordenada x.\n");
+                printf("4) Ordenar listado de puntos por coordenada y.\n");
+                printf("5) Sublistado que satisface cotas en coordenada x.\n");
+                printf("6) Sublistado que satisface cotas en coordenada y.\n");
+                printf("9) Salir del programa.\n");
+                break;
+            case 1:
+                do 
+                {   
+                    printf("-----------------------------------------------\n");
+                    int isOk = leer_archivo(Lista); 
+                } while (isOk == 1);
+                break;
+            case 2:
+                printf("\nIngrese cantidad de puntos a generar: ");
+                scanf("%ld", &n);
+                printf("Generando...\n");
+
+                Lista = (LE *)malloc(sizeof(LE));
+                Lista->init = NULL;
+                Lista->end = NULL;
+
+                for (int i = 0; i < n; i++)
+                {
+                    add_random_element(Lista);
+                }
+
+                printf("Puntos generados correctamente.\n");
+
+                if (Lista->n <= 100)
+                {
+                    printf("Listado: \n");
+                    view_list(Lista);
+                }
+                break;
+            case 3:
+                if (Lista != NULL && Lista->init != NULL)
+                {   
+                    printf("\nResolviendo...\n"); 
+
+                    t0 = clock(); // Inicio medición de tiempo
+                    Quicksort_x(Lista->init, Lista->end);
+                    t1 = clock(); // Fin medición de tiempo
+
+                    if (Lista->n <= 100)
+                    {
+                        printf("Listado ordenado en coordenada x: \n");
+                        view_list(Lista);
+                    }
+                    printf("\nFINALIZADO, TIEMPO TOTAL EN SEGUNDOS: %f  \n", ((t1 - t0) / CLOCKS_PER_SEC)); 
+                }
+                else
+                {
+                    printf("Error: primero debe generar un listado de puntos o leerlo desde un archivo.\n");
+                }
+                break;
+            case 4:
+                if (Lista != NULL && Lista->init != NULL)
+                {   
+                    printf("\nResolviendo...\n"); 
+
+                    t0 = clock(); // Inicio medición de tiempo
+                    Quicksort_y(Lista->init, Lista->end);
+                    t1 = clock(); // Fin medición de tiempo
+
+                    if (Lista->n <= 100)
+                    {
+                        printf("Listado ordenado en coordenada y: \n");
+                        view_list(Lista);
+                    }
+                    printf("\nFINALIZADO, TIEMPO TOTAL EN SEGUNDOS: %f  \n", ((t1 - t0) / CLOCKS_PER_SEC)); 
+                }
+                else
+                {
+                    printf("Error: primero debe generar un listado de puntos o leerlo desde un archivo.\n");
+                }
+                break;
+            case 5:
+                if (Lista != NULL && Lista->init != NULL)
+                {   
+                    printf("\nIngrese cota inferior en x: ");
+                    scanf("%ld", &cota_inf);
+                    printf("\nIngrese cota superior en x: ");
+                    scanf("%ld", &cota_sup);
+                    printf("\nResolviendo...\n"); 
+
+                    t0 = clock(); // Inicio medición de tiempo
+                    LE *subListado = sublistado(Lista, cota_inf, cota_sup, 1);
+                    t1 = clock(); // Fin medición de tiempo
+
+                    if (subListado->n <= 100)
+                    {
+                        printf("Sublistado que satisface las cotas %ld <= x <= %ld \n", cota_inf, cota_sup);
+                        view_list(subListado);
+                    }
+                    printf("\nFINALIZADO, TIEMPO TOTAL EN SEGUNDOS: %f  \n", ((t1 - t0) / CLOCKS_PER_SEC)); 
+                }
+                else
+                {
+                    printf("Error: primero debe generar un listado de puntos o leerlo desde un archivo.\n");
+                }
+                break;
+            case 6:
+                if (Lista != NULL && Lista->init != NULL)
+                {   
+                    printf("\nIngrese cota inferior en y: ");
+                    scanf("%ld", &cota_inf);
+                    printf("\nIngrese cota superior en y: ");
+                    scanf("%ld", &cota_sup);
+                    printf("\nResolviendo...\n"); 
+
+                    t0 = clock(); // Inicio medición de tiempo
+                    LE *subListado = sublistado(Lista, cota_inf, cota_sup, 0);
+                    t1 = clock(); // Fin medición de tiempo
+
+                    if (subListado->n <= 100)
+                    {
+                        printf("Sublistado que satisface las cotas %ld <= y <= %ld \n", cota_inf, cota_sup);
+                        view_list(subListado);
+                    }
+                    printf("\nFINALIZADO, TIEMPO TOTAL EN SEGUNDOS: %f  \n", ((t1 - t0) / CLOCKS_PER_SEC)); 
+                }
+                else
+                {
+                    printf("Error: primero debe generar un listado de puntos o leerlo desde un archivo.\n");
+                }
+                break;
+            case 9:
+                printf("Listo para salir, entrar cualquier valor para terminar: ");
+                scanf("%d",&i);
+                break;
+            default:
+                printf("Seleccion invalida, reintentar (9 para salir): ");
+                break;
+        }
+    } while (sel != 9);
+    
+    /*
     LE *Lista;
     Lista = (LE *)malloc(sizeof(LE));
     Lista->init = NULL;
@@ -57,12 +228,12 @@ int main()
     printf("Lista ordenada en y:\n");
     Quicksort_y(Lista->init, Lista->end);
     view_list(Lista);
-    */
+    
     printf("\n---------------------------\n");
     printf("Sublistado cotas 10<y<50:\n");
     LE *subListado = sublistado(Lista, 10, 50, 0);
     view_list(subListado);
-
+    */
     return 0;
 }
 
@@ -151,6 +322,7 @@ void Quicksort_y(Bloque *left, Bloque *right)
 
 LE *sublistado(LE *lista, long cota_inf, long cota_sup, int x)
 {   
+    int n = 0;
     LE *subListado;
     subListado = (LE *)malloc(sizeof(LE));
     subListado->init = NULL;
@@ -163,19 +335,22 @@ LE *sublistado(LE *lista, long cota_inf, long cota_sup, int x)
         if (x == 1 && elem->x >= cota_inf && elem->x <= cota_sup)  
         {
             add_element(subListado, elem->x, elem->y);
+            n++;
         }
         else if (x == 0 && elem->y >= cota_inf && elem->y <= cota_sup)
         {
             add_element(subListado, elem->x, elem->y);
+            n++;
         }
         
         elem = elem->post;
     }
+    subListado->n = n;
     return subListado;
 }
 
 
-void leer_archivo(LE *lista)
+int leer_archivo(LE *lista)
 {
 
     Bloque *elem1, *elem2;
@@ -187,14 +362,15 @@ void leer_archivo(LE *lista)
     long x, y;
     int n = 0;
 
-    // printf("Ingrese nombre del archivo (con extension, ejemplo: matriz.txt): ");
-    // scanf("%s", nombreArchivo);
+    printf("Ingrese nombre del archivo (con extension, ejemplo: archivo1.txt): ");
+    scanf("%s", nombreArchivo);
 
-    archivo = fopen("archivo2.txt", "r");
+    archivo = fopen(nombreArchivo, "r");
 
     if (archivo == NULL)
     {
-        printf("\nError de apertura del archivo. \n\n");
+        printf("\nError de apertura del archivo.\n");
+        return 0;
     }
     else
     {
@@ -222,12 +398,42 @@ void leer_archivo(LE *lista)
     }
 
     fclose(archivo);
+    printf("\nPuntos cargados correctamente.\n");
+    return 1;
 }
 
 
 void add_element(LE *lista, long x, long y)
 {
     // constructor elemento
+    Bloque *elemento = (Bloque *)malloc(sizeof(Bloque));
+    elemento->x = x;
+    elemento->y = y;
+    elemento->pre = NULL;
+    elemento->post = NULL;
+
+    if (lista->init == NULL)
+    {
+        lista->init = elemento;
+        lista->end = elemento;
+        lista->n = 1;
+    }
+    else
+    {
+        elemento->pre = lista->end;
+        elemento->pre->post = elemento;
+        elemento->post = NULL;
+        lista->end = elemento;
+        lista->n = lista->n + 1;
+    }
+}
+
+
+void add_random_element(LE *lista)
+{
+    // constructor elemento
+    long x = rand()%NMAX;
+    long y = rand()%NMAX;
     Bloque *elemento = (Bloque *)malloc(sizeof(Bloque));
     elemento->x = x;
     elemento->y = y;
